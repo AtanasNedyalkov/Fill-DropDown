d4e37f7a10c279689f5b9c877a8e7acd2041ba95
atanad nedyalkov <atanas.nedyalkov92@gmail.com>
2022-11-23 15:31:01 +0200

    Add project files.

---
+++ dropdown.js
@@ -0,0 +1,45 @@
+import {html, render} from "./node_modules/lit-html/lit-html.js"
+const url = "http://localhost:3030/jsonstore/advanced/dropdown";
+
+const root = document.getElementById("menu");
+const form = document.querySelector("form");
+form.addEventListener("submit", onSubmit)
+
+
+onLoadContent();
+
+async function onLoadContent(){
+    const response = await fetch(url)
+    const data = await response.json();
+
+    const result = Object.values(data).map(x=>createOptionalTemplate(x));
+    render(result,root);
+}
+
+function createOptionalTemplate(option){
+ return html`<option value=${option._id}">${option.text}</option>` 
+}
+
+function onSubmit(e) {
+    e.preventDefault()
+    const value = document.getElementById("itemText").value;
+    addItem(value);
+}
+
+async function addItem(data){
+    const response = await fetch(url,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({text:data})
+
+    })
+}
+
+onLoadContent()
+
+
+
+
+
